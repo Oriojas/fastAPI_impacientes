bf44a16e3bb8d78849d9445c586fe713d054e56b
Oriojas <oscarriojas@gmail.com>
2023-02-24 18:52:23 -0500

    feat: create describe endpoint

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import uvicorn
+import pandas as pd
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Union
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+
+
+class dataFrame(BaseModel):
+    mpg: float
+    cylinders: int
+    displacement: float
+    horsepower: float
+    weight: float
+    acceleration: float
+    model: float
+    year: int
+    origin: int
+    car_name: str
 
 
 @app.get("/hello/")
@@ -26,6 +44,15 @@ async def sum_num(num1: float, num2: float):
     string_result = f"La suma de los dos numéros es: {result}"
 
     return string_result
+
+
+@app.post("/describe/")
+async def describe(data_frame_j: Union[List[dataFrame]]):
+    json_data_input = jsonable_encoder(data_frame_j)
+
+    json_output = jsonable_encoder(json_data_input)
+
+    return json_output
 
 
 if __name__ == '__main__':
