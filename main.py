9da23cf20ae0202052d91a3a90b000f3c98bd351
Oscar Riojas <oscar@whaleandjaguar.co>
2023-03-01 19:50:31 -0500

    feat: two ednpoint

--- main.py
+++ main.py
@@ -7,10 +7,26 @@ from typing import List, Union
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-app = FastAPI()
+app = FastAPI()  # instancia de la app
 
 
 
+
+
+@app.get("/hello/")
+async def hello():
+    greeting = "Hello Word"
+    print(f"{greeting}")
+
+    return greeting
+
+
+@app.get("/hello_you/")
+async def hello_you(name: str):
+    greeting = f"Hello word {name}"
+    print(f"{greeting}")
+
+    return greeting
 
 
 if __name__ == '__main__':
